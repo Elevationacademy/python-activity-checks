ef9231e3454603eb7be231c5125deba8240983fa
Lior Hoshea <hoshealior@gmail.com>
2020-05-03 13:53:00 +0300

    fixed test 3

--- exercises/ex-3_test.py
+++ exercises/ex-3_test.py
@@ -14,10 +14,10 @@ class TestEx3(unittest.TestCase):
         Item = SourceFileLoader("Item", evaluation_loc).load_module().Item
         evaluation_loc = os.path.join(__location__, 'src', 'Instrument.py')
         Instrument = SourceFileLoader("Instrument",evaluation_loc).load_module().Instrument
-        inst = Instrument('Percussion', 'Guitar')
+        inst = Instrument('Percussion', 'Tambourine')
         assert isinstance(inst, Item), 'The Instrument class does not inherit from the Item class'
         assert inst.category == 'Percussion', f"The 'category' of the instrument was not initialized in the constructor. When passing 'Percussion' as an argument to the constructor, the category property was initialized to '{inst.category}' - make sure you're using 'self'"
-        assert inst.type == 'Guitar', f"The 'type' of the instrument was not initialized in the constructor. When passing 'Guitar' as an argument to the constructor, the type property was initialized to '{inst.type}' - make sure you're using 'self'"
+        assert inst.type == 'Tambourine', f"The 'type' of the instrument was not initialized in the constructor. When passing 'Tambourine' as an argument to the constructor, the type property was initialized to '{inst.type}' - make sure you're using 'self'"
         inst.use()
         assert inst.value == 95, 'The use() method of Instrument class does not decrease the value property by 5 when its category equal to "Percussion". Make sure you override the use() method of Item'
         inst.category = 'Woodwind'
