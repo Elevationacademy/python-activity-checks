d462bbc7fd485131ae7025b3383246e3a8b5b5bb
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-16 14:14:13 +0300

    Added test for testing sort and filter formulas.

--- exercises/ex2_test.py
+++ exercises/ex2_test.py
@@ -7,7 +7,17 @@ class TestEx2(unittest.TestCase):
         pass
 
     def test_SortFormula(self):
-        pass
+        self.checkSortFormula('Eighties')
+        self.checkSortFormula('Strategy')
+
+    def checkSortFormula(self, ws_idx):
+        sum_ws = wb_formulas[ws_idx]
+        cell = sum_ws['A2']
+        assert cell.data_type == 'f', f"cell {cell.coordinate} should be a formula"
+        assert 'SORT' in cell.value, f"cell {cell.coordinate} formula should include SORT function"
+        assert 'FILTER' in cell.value, f"cell {cell.coordinate} formula should include FILTER function"
+        assert 'vgsales!' in cell.value, f"cell {cell.coordinate} formula should include refrence to vgsales Tab"
+
 
     def test_EightiesTabTableValues(self):
         pass
