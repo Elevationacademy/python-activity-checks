ff477fd970b4d69c7a58c33f9b7236bd39a7688b
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-16 13:49:15 +0300

    Completed all ex1 tests.

--- exercises/ex1_test.py
+++ exercises/ex1_test.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 from .videoGameWorksheetModel import *
 
@@ -13,10 +14,22 @@ class TestEx1(unittest.TestCase):
         prev_vg = VideoGameDetails(ws[2])
         for row in ws.iter_rows(min_row=3, max_row=row_count - 1):
             cur_vg = VideoGameDetails(row)
-#            print(row[0].row, cur_vg.__dict__)
             assert prev_vg <= cur_vg, f"table is not ordered correctly, see row {row[0].row}"
             prev_vg = cur_vg
 
     def test_GlobalSalesValues(self):
-        pass
+        for row in ws_data.iter_rows(min_row=2, max_row=row_count - 1):
+            cell = row[VideoGameSalesSheetCols.GlobalSales.value]
+            assert cell.data_type == 'n', f"cell {cell.coordinate} does not contain a value "
+            expected_value = row[VideoGameSalesSheetCols.EuSales.value].value + \
+                             row[VideoGameSalesSheetCols.JpSales.value].value + \
+                             row[VideoGameSalesSheetCols.NaSales.value].value + \
+                             row[VideoGameSalesSheetCols.OtherSales.value].value
+            self.assertAlmostEqual(cell.value, expected_value, places=4,
+                                    msg=f"cell {cell.coordinate} value should be {expected_value} but it is {cell.value}")
+
+        # this loop is done separately since using cell_formula[cell.coordinate] inflicts great delays
+        for row in ws.iter_rows(min_row=2, max_row=row_count - 1):
+            cell_formula = row[VideoGameSalesSheetCols.GlobalSales.value]
+            assert cell_formula.data_type == 'f', f"cell {cell_formula.coordinate} should be a formula"
 
