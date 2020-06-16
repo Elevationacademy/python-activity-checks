6cf9bfce9dfa29b2907f7b925b6f8fd51a8fd743
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-16 14:59:08 +0300

    Added ex2 section 2 tests.

--- exercises/ex2_test.py
+++ exercises/ex2_test.py
@@ -4,7 +4,20 @@ from .videoGameWorksheetModel import *
 
 class TestEx2(unittest.TestCase):
     def test_StrategyTabTableValues(self):
-        pass
+        # check on the source data how many Strategy games we have.
+        expected_count = 0
+        for row in ws.iter_rows(min_row=2, max_row=row_count - 1):
+            expected_count += 1 if row[VideoGameSalesSheetCols.Genre.value].value == "Strategy" else 0
+
+        actual_count = 0
+        sum_ws = wb_formulas['Strategy']
+        for row in sum_ws.iter_rows(min_row=2, max_row=sum_ws.max_row - 1):
+            cell = row[VideoGameSalesSheetCols.Genre.value]
+            assert cell.value == 'Strategy', f"row  {cell.row} contains game from year {cell.value}"
+            actual_count += 1
+
+        assert expected_count == actual_count, f"rows count after filter should be {expected_count} but it is {actual_count} "
+
 
     def test_SortFormula(self):
         self.checkSortFormula('Eighties')
