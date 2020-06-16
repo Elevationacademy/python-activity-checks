ca04107cb002c83c1ffeee9e5ed8e84d5ae0c899
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-16 13:21:51 +0300

    Added Ex1 section 2 test.

--- exercises/ex1_test.py
+++ exercises/ex1_test.py
@@ -6,11 +6,17 @@ class TestEx1(unittest.TestCase):
     def test_YearColumn(self):
         for row in ws.iter_rows(min_row=2, max_row=row_count - 1):
             cell = row[VideoGameSalesSheetCols.Year.value]
-            assert cell.data_type == 'n', f"cell {cell.coordinate} should be a numeric"
+            assert cell.data_type == 'n', f"cell {cell.coordinate} does not contain a numeric year value "
+            assert cell.value <= 2016, f"cell {cell.coordinate} contains value from year {cell.value}, this row should be removed. "
 
     def test_TableSort(self):
-        pass
+        prev_vg = VideoGameDetails(ws[2])
+        for row in ws.iter_rows(min_row=3, max_row=row_count - 1):
+            cur_vg = VideoGameDetails(row)
+#            print(row[0].row, cur_vg.__dict__)
+            assert prev_vg <= cur_vg, f"table is not ordered correctly, see row {row[0].row}"
+            prev_vg = cur_vg
 
-    def test_GloablSalesValues(self):
+    def test_GlobalSalesValues(self):
         pass
 
--- exercises/videoGameWorksheetModel.py
+++ exercises/videoGameWorksheetModel.py
@@ -9,27 +9,44 @@ ws = wb_formulas['vgsales']
 ws_data = wb_data['vgsales']
 row_count = ws.max_row
 
-
 class VideoGameSalesSheetCols(enum.Enum):
     Rank = 0
     Name = 1
     Platform = 2
     Year = 3
-    Genre = 5
-    Publisher = 6
-    NaSales = 7
-    EuSales = 8
-    JpSales = 9
-    OtherSales = 10
-    GlobalSales = 11
+    Genre = 4
+    Publisher = 5
+    NaSales = 6
+    EuSales = 7
+    JpSales = 8
+    OtherSales = 9
+    GlobalSales = 10
 
 
-#class VideoGameDetails
-#    def __init__(self, row):
-#        self.Year = row[VideoGameSalesSheetCols.Year].value
-#        self.Genre = row[VideoGameSalesSheetCols.Year].value
-#        self.Platform = row[VideoGameSalesSheetCols.Year].value
-#        self.Publisher = row[VideoGameSalesSheetCols.Year].value
-#        self.Rank = row[VideoGameSalesSheetCols.Year].value
+class VideoGameDetails:
+    def __init__(self, row):
+        self.Year = row[VideoGameSalesSheetCols.Year.value].value
+        self.Genre = row[VideoGameSalesSheetCols.Genre.value].value
+        self.Platform = str(row[VideoGameSalesSheetCols.Platform.value].value)
+        self.Publisher = row[VideoGameSalesSheetCols.Publisher.value].value
+        self.Rank = row[VideoGameSalesSheetCols.Rank.value].value
+
+    def __eq__(self, other):
+        (self.Year, self.Genre, self.Platform, self.Rank, self.Publisher) == (other.Year, other.Genre, other.Platform, other.Rank, other.Publisher)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __le__(self, other):
+        return (self.Year, self.Genre, self.Platform, self.Rank) <= (other.Year, other.Genre, other.Platform, other.Rank)
+
+    def __gt__(self, other):
+        return not self <= other
+
+    def __ge__(self, other):
+        return (self.Year, self.Genre, self.Platform, self.Rank) >= (other.Year, other.Genre, other.Platform, other.Rank)
+
+    def __lt__(self, other):
+        return not self >= other
 
 
