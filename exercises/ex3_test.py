872c3f81b95d8baf05f16adf87f9d171ec579f69
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-17 11:41:28 +0300

    Added Genres count checks.

--- exercises/ex3_test.py
+++ exercises/ex3_test.py
@@ -32,7 +32,25 @@ class TestEx3(unittest.TestCase):
 
 
     def test_GameCount(self):
-        pass
+
+        expected_genres_counts = {}
+        for row in ws.iter_rows(min_row=2, max_row=row_count):
+            cur_genre = row[VideoGameSalesSheetCols.Genre.value].value
+            if cur_genre not in expected_genres_counts.keys():
+                expected_genres_counts[cur_genre] = 1
+            else:
+                expected_genres_counts[cur_genre] += 1
+
+        for row in self.worksheet_data.iter_rows(min_row=2, max_row=self.max_row):
+            cell = row[1]
+            assert cell.data_type == 'n', f"cell {cell.coordinate} type should be numeric"
+            formula_cell = self.worksheet_formula[cell.coordinate]
+            assert formula_cell.data_type == 'f', f"cell {cell.coordinate} should be a formula"
+            assert "COUNTIF" in formula_cell.value, f"cell {cell.coordinate} should include COUNTIF in its formula"
+            genre = row[0].value
+            assert expected_genres_counts[genre] == cell.value, f"Genere {genre} count is {cell.value} but it " \
+                                                                f"should be {expected_genres_counts[genre]}"
+
 
     def test_TotalIncome(self):
         pass
