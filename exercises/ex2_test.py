b3a42b0f1789a786f7c5b2bce06491608428401f
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-24 12:02:14 +0300

    Added testing for sub total type of pivot table.

--- exercises/ex2_test.py
+++ exercises/ex2_test.py
@@ -9,23 +9,25 @@ class TestEx2(unittest.TestCase):
         expected = 3
         assert actual == expected, f"The Employee Worksheet should include {expected} pivot tables, but it include {actual}"
 
-    def assert_pivot_exists(self, pivot_name):
+    def assert_pivot_exists(self, pivot_name, expected_subtotal):
+        p = None
         for pt in ws_data._pivots:
             if pt.name == pivot_name:
-                return pt
-        assert False, f"could not find pivot table named {pivot_name}"
-        ws_src = pt.cache.cacheSource.worksheetSource
-        assert 'EmployeesData' in ws_src.sheet, f"Pivot table {pivot_name} is not referenceing EmployeesData Sheet"
-
+                p = pt
+        assert p is not None, f"could not find pivot table named {pivot_name}"
+        ws_src = p.cache.cacheSource.worksheetSource
+        assert 'EmployeesData' in ws_src.sheet, f"Pivot table {pivot_name} is not referencing EmployeesData Sheet"
+        actual_subtotal = p.dataFields[0].subtotal
+        assert actual_subtotal == expected_subtotal, f"Pivot table {pivot_name} sub total " \
+                                                     f"should be {expected_subtotal} but it {actual_subtotal}"
+        return p
 
     def test_DepartmentsCountPivotTable(self):
-        pt = self.assert_pivot_exists('DepratmentsPersonalPivot')
-
+        pt = self.assert_pivot_exists('DepratmentsPersonalPivot', 'count')
 
 
     def test_AverageSalaryPivotTable(self):
-        pt = self.assert_pivot_exists('AverageSalaryPivot')
-
+        pt = self.assert_pivot_exists('AverageSalaryPivot', 'average')
 
     def test_SalaryBudget(self):
-        pt = self.assert_pivot_exists('SalaryBudgetPivot')
+        pt = self.assert_pivot_exists('SalaryBudgetPivot', 'sum')
