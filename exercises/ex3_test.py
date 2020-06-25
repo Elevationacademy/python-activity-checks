33c814e85717a5aa6d2b6c3e351b15fca1e05518
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-25 09:03:58 +0300

    added check for correct formula.

--- exercises/ex3_test.py
+++ exercises/ex3_test.py
@@ -2,6 +2,7 @@ import openpyxl as oxl
 from .EmployeesWorksheetModel import *
 import unittest
 
+
 class TestEx3(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
@@ -12,18 +13,34 @@ class TestEx3(unittest.TestCase):
     def checkHeader(self, name, col):
         cell = self.emp_ws_data[1][col]
         assert cell.value.strip() == name.strip(), f"expected column header on cell {cell.coordinate} should be {name} " \
-                                   f"but it is {cell.value}"
+                                                   f"but it is {cell.value}"
 
+    def check_col_formula(self, col_no, formula_substr):
+        for row in self.emp_ws_formula.iter_rows(min_row=2, max_row=row_count):
+            cell = row[col_no]
+            assert cell.data_type == 'f', f"cell {cell.coordinate} should be a formula"
+            for f in formula_substr:
+                assert f in cell.value, f"cell {cell.coordinate} formula should include {f} function"
+             #todo: check that it is referencing the same row.
+#            for r in ref_substr:
+#                assert r in cell.value, f"cell {cell.coordinate} formula should reference to {r}"
 
     def test_FirstNameFirstLetter(self):
         self.checkHeader('FirstNameFirstLetter', 6)
+        self.check_col_formula(6, ["LEFT"])
+
 
     def test_LastNameLastLetter(self):
         self.checkHeader('LastNameLastLetter', 7)
+        self.check_col_formula(7, ["RIGHT"])
 
     def test_TrimedEdgesName(self):
         self.checkHeader('TrimedEdgesName', 8)
+        self.check_col_formula(8, ["MID"])
+
 
     def test_FirstLetterIndex(self):
-        self.checkHeader('FirstLetterIndex',9)
+        self.checkHeader('FirstLetterIndex', 9)
+        self.check_col_formula(9, ["FIND"])
+
 
