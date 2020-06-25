ace8a18b80a54f6c98472c6d55c078189f194f9f
Kfir Yehezkel <kfir.ye@gmail.com>
2020-06-25 10:06:30 +0300

    refactor code to use lambda's instead of repeating the same iteration over and over on rows.

--- exercises/ex3_test.py
+++ exercises/ex3_test.py
@@ -25,40 +25,29 @@ class TestEx3(unittest.TestCase):
 #            for r in ref_substr:
 #                assert r in cell.value, f"cell {cell.coordinate} formula should reference to {r}"
 
+    def check_col_value(self, row_calc_func, col):
+        for row in self.emp_ws_data.iter_rows(min_row=2, max_row=row_count):
+            expected = row_calc_func(row)
+            cell = row[col]
+            assert expected == cell.value, f"cell {cell.coordinate} expected value is {expected} but it " \
+                                            f"is actual {cell.value}"
+
     def test_FirstNameFirstLetter(self):
         self.checkHeader('FirstNameFirstLetter', 6)
         self.check_col_formula(6, ["LEFT"])
-        for row in self.emp_ws_data.iter_rows(min_row=2, max_row=row_count):
-            expected = row[2].value[0]
-            cell = row[6]
-            assert expected == cell.value, f"cell {cell.coordinate} expected value is {expected} but it " \
-                                            f"is actual {cell.value}"
+        self.check_col_value(lambda r: r[2].value[0], 6)
 
     def test_LastNameLastLetter(self):
         self.checkHeader('LastNameLastLetter', 7)
         self.check_col_formula(7, ["RIGHT"])
-        for row in self.emp_ws_data.iter_rows(min_row=2, max_row=row_count):
-            expected = row[3].value[-1]
-            cell = row[7]
-            assert expected == cell.value, f"cell {cell.coordinate} expected value is {expected} but it " \
-                                            f"is actual {cell.value}"
+        self.check_col_value(lambda r: r[3].value[-1], 7)
 
     def test_TrimedEdgesName(self):
         self.checkHeader('TrimedEdgesName', 8)
         self.check_col_formula(8, ["MID"])
-        for row in self.emp_ws_data.iter_rows(min_row=2, max_row=row_count):
-            expected = row[2].value[1:-1] + " " + row[3].value[1:-1]
-            cell = row[8]
-            assert expected == cell.value, f"cell {cell.coordinate} expected value is {expected} but it " \
-                                            f"is actual {cell.value}"
+        self.check_col_value(lambda r: r[2].value[1:-1] + " " + r[3].value[1:-1], 8)
 
     def test_FirstLetterIndex(self):
         self.checkHeader('FirstLetterIndex', 9)
         self.check_col_formula(9, ["FIND"])
-        for row in self.emp_ws_data.iter_rows(min_row=2, max_row=row_count):
-            expected = row[3].value.lower().find(row[2].value[0].lower()) + 1
-            cell = row[9]
-            assert expected == cell.value, f"cell {cell.coordinate} expected value is {expected} but it " \
-                                            f"is actual {cell.value}"
-
-
+        self.check_col_value(lambda r: r[3].value.lower().find(r[2].value[0].lower()) + 1, 9)
