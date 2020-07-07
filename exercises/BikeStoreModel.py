dd42f56e65a97d26474f6127b82c4b02972aaa55
Kfir Yehezkel <kfir.ye@gmail.com>
2020-07-07 17:45:56 +0300

    fixed for working with google sheet.

--- exercises/BikeStoreModel.py
+++ exercises/BikeStoreModel.py
@@ -1,13 +1,13 @@
 import enum
 import openpyxl as oxl
 
-workbook_file = '../Solution/BikeStoreSample.xlsx'
+workbook_file = '/home/excel.xlsx'
 
 wb_data = oxl.load_workbook(workbook_file, read_only=False, data_only=True)
 wb_formulas = oxl.load_workbook(workbook_file, read_only=True, data_only=False)
 ws = wb_formulas['OrderDetailsData']
 ws_data = wb_data['OrderDetailsData']
-row_count = ws.max_row
+row_count = ws_data.max_row
 
 
 class BikeStoreSheetCols(enum.Enum):
